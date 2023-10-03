f5e79cdc860f2b8c07649c2baed375cd051c9bc4
DeepaliP98 <deepali846@gmail.com>
2023-10-03 15:32:16 +0200

    Reader initial commit

---
+++ data/readers/BaseDataReader.py
@@ -0,0 +1,15 @@
+from transformers import DPRReader, DPRReaderTokenizer
+
+class Reader:
+    def __int__(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def train(self):
+        pass
+
+    def load(self,model_path,is_pretrained=True):
+        pass
+
