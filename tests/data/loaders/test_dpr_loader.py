d9aec1d3accf6fd0757b9ace7a4acba345cc3e63
DeepaliP98 <deepali846@gmail.com>
2023-11-14 17:22:14 +0100

    add prefix and seperators

--- tests/data/loaders/test_dpr_loader.py
+++ tests/data/loaders/test_dpr_loader.py
@@ -1,5 +1,4 @@
 import unittest
-
 from constants import Split
 from data.datastructures.dataset import DprDataset
 from data.datastructures.sample import Sample
@@ -14,7 +13,7 @@ class MyTestCase(unittest.TestCase):
                                      document_encoder_path="facebook-dpr-ctx_encoder-single-nq-base",
                                      ann_search="annoy_search",
                                      num_negative_samples=5)
-        loader = DprDataLoader("nq-data", config_path="test_dpr_config.ini",config=config_instance, split=Split.DEV, batch_size=10)
+        loader = DprDataLoader("nq-data", config_path="tests/data/loaders/test_dpr_config.ini",config=config_instance, split=Split.DEV, batch_size=10)
         assert len(loader.raw_data) == len(loader.dataset)
         self.assertTrue(isinstance(loader.dataset, DprDataset))
         self.assertTrue(isinstance(loader.tokenizer, Tokenizer))
