7f57866575c617fa0d7c18d9faf624926e34d5ed
DeepaliP98 <deepali846@gmail.com>
2023-11-28 09:46:39 +0100

    Remove raw file

--- retriever/raw.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoTokenizer, AutoModel
-
-questions = ["Hi what is your name?"]
-tokenizer = AutoTokenizer.from_pretrained("castorini/tct_colbert-v2-hnp-msmarco")
-model = AutoModel.from_pretrained("castorini/tct_colbert-v2-hnp-msmarco")
-
-tokenized_questions = tokenizer(questions, padding=True, truncation=True, return_tensors='pt')
-token_emb =  model(**tokenized_questions)
-print(token_emb)
